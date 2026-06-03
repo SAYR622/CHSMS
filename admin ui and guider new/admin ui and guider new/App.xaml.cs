461d907e3fb6774651c45695a8b95b5563928660
sandikahiman <ashweerasooriya@students.nsbm.ac.lk>
2026-06-03 19:39:05 +0530

    Add files via upload

---
+++ admin ui and guider new/admin ui and guider new/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace admin_ui_and_guider_new
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
