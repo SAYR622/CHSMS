80ab59e782520275273fc71efd9484bd0ac3c3c1
sandikahiman <ashweerasooriya@students.nsbm.ac.lk>
2026-06-04 14:46:54 +0530

    Add files via upload

---
+++ visitor Profile/visitor Profile/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace visitor_Profile
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
