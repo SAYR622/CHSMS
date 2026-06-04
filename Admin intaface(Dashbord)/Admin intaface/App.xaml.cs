5adf2ac2206d27cd01636bfecbe8b5448cd8cbf4
sandikahiman <ashweerasooriya@students.nsbm.ac.lk>
2026-06-04 14:50:57 +0530

    Add files via upload

---
+++ Admin intaface(Dashbord)/Admin intaface/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Admin_intaface
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
